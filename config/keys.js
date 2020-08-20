5a7a75419f1ab1e08c2f68f393b80a22fb411013
AgilianPC <agilian.lucas@hotmail.com>
2020-08-20 00:10:53 -0300

    a11

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://deploy:' +  encodeURIComponent('filsoldeploy') + '@cluster0.mivpe.mongodb.net/filsol?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://deploy:' +  encodeURIComponent('filsoldeploy') + '@cluster0.mivpe.mongodb.net/userexample?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
